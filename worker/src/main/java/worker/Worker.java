04131605ef20545632d641b57cb7364068e5bcdb
Lucas Paula <luolcami@gmail.com>
2022-12-30 12:00:24 +0000

    added newline

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -19,6 +19,7 @@ class Worker {
         String voterID = voteData.getString("voter_id");
         String vote = voteData.getString("vote");
 
+        
         System.err.printf("Processing vote for '%s' by '%s'\n", vote, voterID);
         updateVote(dbConn, voterID, vote);
       }
